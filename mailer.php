d00b958f4355bf1d1e8298888ef6340dde54225a
Paolo Falco <paolo.falcoapp@gmail.com>
2023-10-07 10:46:40 +0200

    commit 8 - create mailer (mailtrap)

---
+++ mailer.php
@@ -0,0 +1,29 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'vendor/autoload.php'; // Se stai usando Composer, altrimenti regola il percorso correttamente
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth   = true;
+
+$mail->Host       = 'smtp.mailtrap.io'; // Hostname di Mailtrap
+$mail->Username   = 'c36d63613c96fe'; // Nome utente di Mailtrap
+$mail->Password   = 'acc353fd83353f'; // Password di Mailtrap
+$mail->SMTPSecure = 'tls';
+$mail->Port       = 2525; // Porta SMTP di Mailtrap
+
+$mail->isHTML(true);
+
+return $mail;
+    
+    
+
+ 
+    
+
